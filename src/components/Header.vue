3b5343ed8f9f97e1336dd79c8ba3f13b7dcb20b8
Matheus <matheustrilhakoch@hotmail.com>
2024-07-07 18:43:49 -0300

    Adicionado google fonts para header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,37 +1,19 @@
 <script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
-})
+
 </script>
 
 <template>
-  <div>Header</div>
+  <div class="titulo">Weather App</div>
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
+  .titulo {
+    font-family: "Open Sans", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 300;
+    font-style: normal;
+    font-variation-settings: "wdth" 86.5;
+    font-size: large;
+    
   }
-}
 </style>
