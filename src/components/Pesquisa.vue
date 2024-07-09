1fea1baa42ca4726ea2c421f2c6b0b2eb76d2528
Matheus <matheustrilhakoch@hotmail.com>
2024-07-08 21:55:35 -0300

    Iniciando pesquisa

--- src/components/Pesquisa.vue
+++ src/components/Pesquisa.vue
@@ -3,5 +3,15 @@
 </script>
 
 <template>
-  <div>Pesquisa</div>
+  <div class="pesquisa">
+    <input type="text" placeholder="Busca...">
+  </div>
 </template>
+
+<style scoped>
+  .pesquisa {
+    position: absolute;
+    left: 50%;
+    top: 15%;
+  }
+</style>
