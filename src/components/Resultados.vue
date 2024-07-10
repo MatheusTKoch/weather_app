f57ca8fecd3e443516dc3b129324be9ee521a592
Matheus <matheustrilhakoch@hotmail.com>
2024-07-09 22:34:02 -0300

    Ajustando card trasnparente

--- src/components/Resultados.vue
+++ src/components/Resultados.vue
@@ -1,10 +1,27 @@
 <template>
-  <div class="resultados">Resultados</div>
+  <div class="card">
+    <div class="resultados">Resultados</div>
+  </div>
 </template>
 
 <style scoped>
   .resultados {
     position: absolute;
     bottom: 25%;
+    left: 43%;
+  }
+
+  .card {
+    position: absolute;
+    bottom: 20%;
+    left: 42%;
+    width: 250px; 
+    box-shadow: 0 15px 25px rgba(129, 124, 124, 0.2); 
+    height: 300px; 
+    border-radius: 1px; 
+    backdrop-filter: blur(2px); 
+    background-color: rgba(255, 255, 255, 0.2); 
+    padding: 10px; 
+    text-align: center; 
   }
 </style>
