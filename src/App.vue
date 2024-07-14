b41a37cf4a2ec61ae2fb22f81e88574b76a670e3
Matheus <matheustrilhakoch@hotmail.com>
2024-07-14 18:58:17 -0300

    Ajustando app

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <script setup>
+import { ref } from "vue";
 import Header from './components/Header.vue';
 import Pesquisa from './components/Pesquisa.vue';
 import Resultados from './components/Resultados.vue';
+
+const cidade = ref(null);
+
+function handleCidade(param) {
+  cidade.value = param;
+}
 </script>
 
 <template>
   <Header></Header>
-  <Pesquisa></Pesquisa>
-  <Resultados></Resultados>
+  <Pesquisa @cidade="handleCidade"></Pesquisa>
+  <Resultados :cidade="cidade"></Resultados>
 </template>
 
 <style scoped>
