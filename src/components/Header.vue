f5e6143c4c925c2952c8dc05fdad985d45c23410
Matheus <matheustrilhakoch@hotmail.com>
2024-07-13 21:58:14 -0300

    Ajustando header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,7 +10,14 @@
 
 <style scoped>
   .header {
-    color: black;
+    position: absolute;
+    width: 100vw;
+    left: 0%;
+    top: 0%;
+    box-shadow: 0 15px 25px rgba(129, 124, 124, 0.2);
+    backdrop-filter: blur(2px); 
+    background-color: rgba(255, 255, 255, 0.2); 
+    padding-bottom: 5%;
   }
   .titulo {
     font-family: "Open Sans", sans-serif;
