094129b220a4da15cc8712e85e5a2e171dec2cc2
Matheus <matheustrilhakoch@hotmail.com>
2024-07-14 21:13:54 -0300

    Ajustando rotas

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import PesquisaView from '@/views/PesquisaView.vue'
+import App from '@/App.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: PesquisaView
+      component: App
     },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // }
+    {
+      path: '/resultado/:cidade',
+      name: 'Resultado',
+      component: Resultado,
+      props: true,
+    }
   ]
 })
 
