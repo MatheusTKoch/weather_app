2896b5e27dfd35040c01dcc9489f63f78aaf1822
Matheus <matheustrilhakoch@hotmail.com>
2024-07-23 22:42:09 -0300

    Ajustando imports do pyhton

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify; 
+from flask import Flask, jsonify, request; 
 import requests;
 from flask_cors import CORS;
 import os;
@@ -18,14 +18,14 @@ def ping_pong():
 
 @app.route('/resultado', methods=['GET'])
 def get_weather():
-    city_name = requests.args.get('city')
-    state_code = requests.args.get('state_code', '')
-    country_code = requests.args.get('country_code', '')
-    limit = requests.args.get('limit', 1)
-    lang = requests.args.get('lang', 'pt_br')
+    city_name = request.args.get('city')
+    state_code = request.args.get('state_code', '')
+    country_code = request.args.get('country_code', '')
+    limit = request.args.get('limit', 1)
+    lang = request.args.get('lang', 'pt_br')
 
     get_string = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&limit={limit}&appid={API_KEY}"
-    getCall = requests.get(get_string)
+    getCall = request.get(get_string)
     if getCall.ok:
         responseCall = getCall.json()
         cityName = responseCall[0]['name']
@@ -33,7 +33,7 @@ def get_weather():
         longitude = responseCall[0]['lon']
 
         forecast_string = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={API_KEY}&units=metric&lang={lang}"
-        forecastCall = requests.get(forecast_string)
+        forecastCall = request.get(forecast_string)
         responseForecast = forecastCall.json()
         description = responseForecast['weather'][0]['description']
         minTemp = responseForecast['main']['temp_min']
