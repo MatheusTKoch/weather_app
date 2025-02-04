bc298f3828d7b0f22765956243296dfdbe4d8157
Matheus <matheustrilhakoch@hotmail.com>
2025-02-03 21:08:18 -0300

    Ajuste no web history

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from '@/App.vue'
 import Resultados from '@/components/Resultados.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
